95713df943a1b083175da6f06cb6ea45feca07e1
Piotr Wolski <w.piotr94@gmail.com>
2018-03-29 16:31:46 +0200

    segmentacja

--- nn-sharpen-v1.py
+++ nn-sharpen-v1.py
@@ -3,22 +3,18 @@ import sys
 import time
 
 import PIL
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from PIL import Image
 
 from ai.conv import ImageOperations
 
-min = 0
-max = 0
-
-
 class NN_Sharpen:
-    def __init__(self):
+    def __init__(self, height, width):
         self.learning_rate = 0.001
-        self.batch_size = 5
-        self.input_width = 430
-        self.input_height = 611
+        self.batch_size = 1
+        self.rezize_factor = 4
+        self.input_width = int(width / self.rezize_factor)
+        self.input_height = int(height / self.rezize_factor)
         self.layer_info = []
 
     def push_conv(self, name, shape, downsample, activation, in_layer):
@@ -30,7 +26,7 @@ class NN_Sharpen:
         self.layer_info.append((W, in_layer.get_shape().as_list(), downsample))
         return out_layer
 
-    def pop_conv(self, name, shape, upsample, activation, in_layer):
+    def pop_conv(self, name, shape, activation, in_layer):
         conv_shape = self.layer_info.pop()
 
         W = tf.get_variable("W_" + name,
@@ -39,41 +35,42 @@ class NN_Sharpen:
 
         layer = activation(tf.add(
             tf.nn.conv2d_transpose(in_layer, W, tf.stack(conv_shape[1]), [1, conv_shape[2], conv_shape[2], 1],
-                                   padding="SAME")
-            , B), name="DECONV_" + name)
+                                   padding="SAME"), B), name="DECONV_" + name)
         return layer
 
     def last_layer(self, shape, in_layer, upsample):
         W2 = tf.get_variable("W_out", initializer=tf.truncated_normal(shape, stddev=0.1))
         B2 = tf.get_variable("B_out", initializer=tf.ones([3]) / 10)
         layer = tf.nn.relu(tf.add(tf.nn.conv2d_transpose(in_layer, W2,
-                                                         [5, self.input_width, self.input_height, 3],
+                                                         [self.batch_size, self.input_width, self.input_height, 3],
                                                          strides=[1, upsample, upsample, 1],
-                                                         padding='SAME')
-                                  , B2), name="Y")
+                                                         padding='SAME'), B2), name="Y")
         return layer
 
     def train_on_images(self, train, validate):
 
-        image, label, name = self.make_file_pipeline(train, validate, batch_size=5)
+        image, label, name = self.make_file_pipeline(train, validate)
 
         start = time.time()
-        self.sess = tf.Session()
+
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1)  # define amount of GPU memory
+        self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
         # network set-up
-        self.img_label = tf.placeholder(tf.float32, shape=[5, self.input_width, self.input_height, 3],
+        self.img_label = tf.placeholder(tf.float32, shape=[self.batch_size, self.input_width, self.input_height, 3],
                                         name="img_label")
-        self.input = tf.placeholder(tf.float32, shape=[5, self.input_width, self.input_height, 3], name="input")
+        self.input = tf.placeholder(tf.float32, shape=[self.batch_size, self.input_width, self.input_height, 3],
+                                    name="input")
 
         # f_sizeX, fsizeY, in channels, out channels
-        layer = self.push_conv("1", [5, 5, 3, 16], 2, tf.nn.leaky_relu, self.input)
-        layer = self.push_conv("2", [3, 3, 16, 32], 1, tf.nn.leaky_relu, layer)
-        layer = self.push_conv("3", [3, 3, 32, 32], 1, tf.nn.leaky_relu, layer)
-        layer = self.push_conv("4", [3, 3, 32, 32], 1, tf.nn.leaky_relu, layer)
+        layer = self.push_conv("1", [5, 5, 3, 8], 2, tf.nn.leaky_relu, self.input)
+        layer = self.push_conv("2", [3, 3, 8, 16], 1, tf.nn.leaky_relu, layer)
+        layer = self.push_conv("3", [3, 3, 16, 16], 1, tf.nn.leaky_relu, layer)
+        layer = self.push_conv("4", [3, 3, 16, 16], 1, tf.nn.leaky_relu, layer)
 
-        layer = self.pop_conv("d1", [3, 3, 32, 32], 1, tf.nn.leaky_relu, layer)
-        layer = self.pop_conv("d2", [3, 3, 32, 32], 1, tf.nn.leaky_relu, layer)
-        layer = self.pop_conv("d3", [3, 3, 16, 32], 1, tf.nn.leaky_relu, layer)
-        self.Y = self.last_layer([5, 5, 3, 16], layer, 2)
+        layer = self.pop_conv("d1", [3, 3, 16, 16], tf.nn.leaky_relu, layer)
+        layer = self.pop_conv("d2", [3, 3, 16, 16], tf.nn.leaky_relu, layer)
+        layer = self.pop_conv("d3", [3, 3, 8, 16], tf.nn.leaky_relu, layer)
+        self.Y = self.last_layer([5, 5, 3, 8], layer, 2)
 
         # train_cost = tf.reduce_sum(tf.square(self.Y - self.img_label))
         train_cost = tf.losses.mean_squared_error(self.Y, self.img_label)
@@ -97,7 +94,7 @@ class NN_Sharpen:
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=self.sess, coord=coord)
 
-        indices = list(range(len(validate)))
+        # indices = list(range(len(validate)))
         i = 0
 
         while True:
@@ -132,8 +129,9 @@ class NN_Sharpen:
 
             log.add_summary(summaries_val, i)
 
-            if i % 10 == 0:
+            if i % 100 == 0:
                 name_of_file = self.get_name(im_lab[2][0])
+
                 self.save_image(im_lab[0], i, name_of_file)
 
             if i % 500 == 0:
@@ -144,30 +142,34 @@ class NN_Sharpen:
 
     def save_image(self, image, iter, name):
 
+        # test = self.sess.run(image_to_save)
+
         test_image = self.sess.run(self.Y, feed_dict={
             self.input: image
         })[0]
-        ImageOperations.saveFile(test_image,
+        image_to_save = tf.image.resize_images(
+            test_image,
+            [self.input_width * self.rezize_factor, self.input_height * self.rezize_factor]
+        )
+
+        test = self.sess.run(image_to_save)
+
+        ImageOperations.saveFile(test,
                                  ".\\{}.jpg".format("data\\" + "test\\" + "blurred_iter_" + str(
                                      iter) + "_name_" + name))
 
-    def get_name(self, name):
+    @staticmethod
+    def get_name(name):
         label_idx = name.decode("utf-8")
         idx_of_name = label_idx.rfind('\\')
         label_idx = label_idx[idx_of_name + 1:-4]
         return label_idx
 
-    def get_before_name(self, name):
+    @staticmethod
+    def get_before_name(name):
         idx_of_name = name.rfind('\\')
         label_idx = name[:idx_of_name + 1]
         return label_idx
-
-    def show_image(self, img):
-        # out_resized = img.reshape((img.shape[0], img.shape[1], 3))
-        plt.imshow(img[:, :, :])
-        figManager = plt.get_current_fig_manager()
-        figManager.window.showMaximized()
-        plt.show()
 
     def sharpen(self, path):
 
@@ -191,7 +193,7 @@ class NN_Sharpen:
         image = tf.to_float(tf.image.decode_jpeg(image, channels=3)) / 256.0
 
         image = tf.reshape(image, [self.input_width, self.input_height, 3])
-        image_batch = tf.train.batch([image], batch_size=5, capacity=1)
+        image_batch = tf.train.batch([image], batch_size=self.batch_size, capacity=1)
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=self.sess, coord=coord)
@@ -222,9 +224,7 @@ class NN_Sharpen:
     def save_model(self, path):
         self.saver.save(self.sess, path + ".ckpt")
 
-    def make_file_pipeline(self, image_files, label_files, batch_size=None, shuffle=True):
-        if batch_size == None:
-            batch_size = self.batch_size
+    def make_file_pipeline(self, image_files, label_files, shuffle=True):
 
         image_files_prod = tf.train.string_input_producer(image_files, shuffle=shuffle, seed=1)
         label_files_prod = tf.train.string_input_producer(label_files, shuffle=shuffle, seed=1)
@@ -234,14 +234,25 @@ class NN_Sharpen:
         image_file, image = reader.read(image_files_prod)
         label_file, label = reader.read(label_files_prod)
 
-        image = tf.to_float(tf.image.decode_jpeg(image, channels=3)) / 256.0
-        label = tf.to_float(tf.image.decode_jpeg(label, channels=3)) / 256.0
+        image = tf.to_float(tf.image.decode_png(image, channels=3)) / 256.0
+        label = tf.to_float(tf.image.decode_png(label, channels=3)) / 256.0
 
-        image = tf.reshape(image, [self.input_width, self.input_height, 3])
-        label = tf.reshape(label, [self.input_width, self.input_height, 3])
+        image = tf.image.resize_images(
+            image,
+            [self.input_width, self.input_height]
+        )
 
-        image_batch, label_batch, image_file_batch = tf.train.batch([image, label, image_file], batch_size=batch_size,
-                                                                    capacity=1300)
+        label = tf.image.resize_images(
+            label,
+            [self.input_width, self.input_height]
+        )
+
+        # image = tf.reshape(image, [self.input_width, self.input_height, 3])
+        # label = tf.reshape(label, [self.input_width, self.input_height, 3])
+
+        image_batch, label_batch, image_file_batch = tf.train.batch([image, label, image_file],
+                                                                    batch_size=self.batch_size,
+                                                                    capacity=2510)
 
         return image_batch, label_batch, image_file_batch
 
@@ -261,27 +272,13 @@ def badSyntax():
 
 
 def main():
-    nn = NN_Sharpen()
     test_file = ""
     load_model = ".\\saved_model\\model"
     tf.reset_default_graph()
     dir_teacher = '.\\data\\faces_resized\\'
     dir_student = '.\\data\\blurred_faces_2\\'
-    num_of_images = 0
     train = False
-    # i = 0
-    # for path in validate_paths:
-    #
-    #     blurred_image = Image.open(path).filter(ImageFilter.BLUR)
-    #     new_path = ".\\data\\blurred_faces\\" + str(i) + ".jpg"
-    #     blurred_image.save(new_path)
-    #     i = i + 1
 
-    # train_paths = getPathsFromDir(dir_student)
-
-    # for i in interval:
-    #     new_path = ".\\data\\new_blur\\" + iter[i] + "_blurred.jpg"
-    #     train_paths.append(new_path)
     validate_paths = []
     train_paths = []
     args = sys.argv
@@ -319,9 +316,11 @@ def main():
             badSyntax()
 
         train = True
+        extension = ".png"
         num_of_images = len(getPathsFromDir(dir_teacher))
-        validate_paths = [dir_teacher + str(i) + ".jpg" for i in range(num_of_images)]
-        train_paths = [dir_student + str(i) + ".jpg" for i in range(num_of_images)]
+        nums = ["%#05d" % num for num in range(1, num_of_images)]
+        validate_paths = [dir_teacher + str(i) + extension for i in nums]
+        train_paths = [dir_student + str(i) + extension for i in nums]
     elif args[2] == "load":
         if args[3] == "--model":
             load_model = args[4]
@@ -333,11 +332,19 @@ def main():
         badSyntax()
 
     if train:
+        nn = init_net(train_paths[0])
         nn.train_on_images(train_paths, validate_paths)
         nn.save_model(".\\saved_model\\model")
     else:
+        nn = init_net(test_file)
         nn.load_model(load_model + "model")
         nn.sharpen(test_file)
+
+
+def init_net(path_to_image):
+    im = Image.open(path_to_image)
+    width, height = im.size
+    return NN_Sharpen(width, height)
 
 
 if __name__ == "__main__":
